e1bd3433704f48639d5ae26b7538387a6e657669
SpencersFault <SpencersFault@users.noreply.github.com>
2019-10-03 12:59:23 -0400

    gchgfhfg

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Player
+{
+    public string playername;
+
+    public float baseHP;
+    public float currentHP;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
